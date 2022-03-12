607e6a5638abefed5943aacb56caddc2bbcce78c
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2022-03-12 15:32:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 let arr = []
-window.navigator.mediaDevices.enumerateDevices()
-.then(devc => {
-    let obj = ''
-    devc.map(device => {
-        console.log(device);
-        obj += `Kind: ${device.kind}\nId: ${device.groupId}\n\n`
-        if(device.kind == 'videoinput')
-            arr.push(device.groupId)
-    })
-})
 
 let video = document.getElementById('video')
 let Stream
-
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
-    navigator.mediaDevices.getUserMedia({video:true})
-    .then(stream => {
-        video.srcObject = stream
-        video.play()
-    })
-}
-
+let spinner = true
 function ChangeCamera() {
-    navigator.mediaDevices.getUserMedia({video:{facingMode:{ exact: "environment" }}})
-    .then(stream => {
-        video.srcObject = stream
-        video.play()
-    })
-    .catch(err => {
-        alert("No rear camera found\n");
-    })
+    if (spinner)
+        Camera('user')
+    else
+        Camera({ exact: "environment" })
 }
+
+function Camera(cam_object) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: cam_object } })
+            .then(stream => {
+                video.srcObject = stream
+                video.play()
+                spinner = !spinner
+            })
+            .catch(err => {
+                alert("Requested Camera not found!");
+            })
+    }
+}
+
+ChangeCamera()
+
+// window.navigator.mediaDevices.enumerateDevices()
+//     .then(devc => {
+//         let obj = ''
+//         devc.map(device => {
+//             console.log(device);
+//             obj += `Kind: ${device.kind}\nId: ${device.groupId}\n\n`
+//             if (device.kind == 'videoinput')
+//                 arr.push(device.groupId)
+//         })
+//     })
